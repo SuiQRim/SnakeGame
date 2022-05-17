88e0d5d7d7320628537e5309c822014053f82c44
Roman Samadi <romikkimor123@gmail.com>
2022-05-17 18:53:10 +0300

    Добавьте файлы проекта.

---
+++ Map.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class Map
+    {
+        private int _width;
+        private int _height;
+        private TimeSpan _dis
+
+    }
+}
---
+++ Skin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class Skin
+    {
+        public Skin(char mainDecor, char sideDecor)
+        {
+            _mainDecoration = mainDecor;
+            _sideDecoration = sideDecor;
+        }
+        private char _mainDecoration;
+        private char _sideDecoration;
+
+        public char MainDecoration
+        {
+            get => _mainDecoration; 
+            set => _mainDecoration = value; 
+
+        }
+        public char SideDecoration
+        {
+            get => _sideDecoration;
+            set => _sideDecoration = value;
+        }
+
+    }
+}
