38ec27a98fb13d51d723ed5111bee61c5a1d4bb1
Roman Samadi <romikkimor123@gmail.com>
2022-05-17 18:59:53 +0300

    Добавлены пустые методы для работы с основными механиками игры. Передвижение обьекта, спавн поинта ( ягоды ), вывод карты

---
+++ Scene.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class Scene
+    {
+        private int _width;
+        private int _height;
+        private TimeSpan _roundTime;
+
+        private int [] _map;
+        public int [] Map
+        {
+            get { return _map; }
+        }
+
+        private void SpawnPoint() 
+        {
+        
+        }
+        private void EnemyMove() 
+        {
+        
+        }
+        
+        private string PrintMap() { return ""; }
+    }
+}
